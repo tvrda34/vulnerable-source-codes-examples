1521e48236fdc05fe9a40912a02a785ba8f94236
tvrda34 <tvrtko.ivasic3@gmail.com>
2025-04-11 17:41:00 +0200

    folder 4 sample fixed

--- python/4/sample.py
+++ python/4/sample.py
@@ -1,15 +1,16 @@
+import os
+from tempfile import TemporaryDirectory
+from subprocess import run, DEVNULL, PIPE
+from flask import Flask, make_response, request
+
+app = Flask(__name__)
 def _git(cmd, args, cwd='/'):
-    proc = run(['git', cmd, *args],
-               stdout=PIPE,
-               stderr=DEVNULL,
-               cwd=cwd,
-               timeout=5)
+    proc = run(['git', cmd, *args], stdout=PIPE, stderr=DEVNULL, cwd=cwd, timeout=5)
     return proc.stdout.decode().strip()
 
 @app.route('/blame', methods=['POST'])
 def blame():
-    url = request.form.get('url',
-                           'https://github.com/package-url/purl-spec.git')
+    url = request.form.get('url', 'https://github.com/package-url/purl-spec.git')
     what = request.form.getlist('what[]')
     with TemporaryDirectory() as local:
         if not url.startswith(('https://', 'http://')):
@@ -20,4 +21,11 @@ def blame():
             file, lines = i.split(':')
             res.append(_git('blame', ['-L', lines, file], local))
         return make_response('\n'.join(res), 200)
+    
+@app.route('/test')
+def test():
+    res = 200 if os.path.exists('/pwned') else 404
+    return make_response('', res)
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
